c3630236fb11e7ce5da6f49d0da51402dede8952
Ragnus <jakub7249@tutanota.com>
2019-10-22 23:36:06 +0200

    some Initial drawing

--- FractalPro/Fractal/Program.cs
+++ FractalPro/Fractal/Program.cs
@@ -1,17 +1,24 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
 namespace Fractal
 {
+    /// <summary>
+    /// Draw Fractal using rule
+    /// </summary>
     class Program
     {
+        //up,right,down,left
+        public static bool[] iterateAxis = new bool[4] { false, true, false, false };
+
         static void Main(string[] args)
         {
-            int iteration = 3;
-            string initialString = "B";
+            int iteration = 4;
+            string initialString = "F+F+F+F";
             Dictionary<char, string> rules = new Dictionary<char, string>();
             rules.Add('B', "BWB"); rules.Add('W', "WWW"); rules.Add('F', "F+F-F-FF+F+F-F");
             StringBuilder sB = new StringBuilder();
@@ -30,37 +37,92 @@ namespace Fractal
                 sB = new StringBuilder();
             }
 
-            Console.WriteLine(initialString);
-            Console.ReadKey();
+
+            Bitmap mainImage = new Bitmap(5000, 5000);
+            int currentX = 300;
+            int currentY = 300;
+
+            //up,right,down,left
+
+            int iterate = 10;
 
 
-            //for (int steps = 1; steps < iteration; steps++)
-            //{
-            //    StringBuilder sB = new StringBuilder();
+            Graphics newGraphics = Graphics.FromImage(mainImage);
+            newGraphics.Clear(Color.White); //Set background color to white
+            using (Graphics g = Graphics.FromImage(mainImage))
+            {
 
-            //    foreach (var item in initialStringSubString)
-            //    {
-            //        if (rules.ContainsKey(Convert.ToChar(item)))
-            //        {
-            //            sB.Append(rules[Convert.ToChar(item)]);
-            //            sB.Append("|");
-            //        }
-            //        else
-            //            sB.Append(rules[Convert.ToChar(item)]);
-            //    }
+                for (int i = 0; i < initialString.Length; i++)
+                {
+                    if (initialString[i] == 'F')
+                    {
+                        for (int iA = 0; iA < iterateAxis.Length; iA++)
+                        {
+                            //up
+                            if (iA == 0 && iterateAxis[iA])
+                            {
+                                g.DrawLine(new Pen(Color.Black), currentX, currentY, currentX, currentY - iterate);
+                                currentY -= iterate;
+                            }
+                            //right
+                            else if(iA == 1 && iterateAxis[iA])
+                            {
+                                g.DrawLine(new Pen(Color.Black), currentX, currentY, currentX + iterate, currentY);
+                                currentX += iterate;
+                            }
+                            //down
+                            else if (iA == 2 && iterateAxis[iA])
+                            {
+                                g.DrawLine(new Pen(Color.Black), currentX, currentY, currentX , currentY + iterate);
+                                currentY += iterate;
+                            }
+                            //left
+                            else if (iA == 3 && iterateAxis[iA])
+                            {
+                                g.DrawLine(new Pen(Color.Black), currentX, currentY, currentX - iterate, currentY);
+                                currentX -= iterate;
+                            }
+                        }
+                    }
+                    else if (initialString[i] == '+')
+                    {
+                        changeAxis(false);
+                    }
+                    else if (initialString[i] == '-')
+                    {
+                        changeAxis(true);
+                    }
+                }
+            }
 
-            //    //for (int i = 0; i < initialString.Length; i++)
-            //    //{
-            //    //    if(rules.ContainsKey(initialString[i]))
-            //    //        sB.Append(rules[initialString[i]]);
-            //    //}
+            mainImage.Save(@"C:\Users\Ragnus\Desktop\PI\Fractal\FractalPro\Fractal\yos.jpg");
+        }
 
-            //    //initialString = sB.ToString();
-            //    //sB = new StringBuilder();
-            //}
+        private static void changeAxis(bool nextAxis)
+        {
+            for (int i = 0; i < iterateAxis.Length; i++)
+            {
+                if (iterateAxis[i] && nextAxis)
+                {
+                    iterateAxis[i] = false;
+                    if (i + 1 >= iterateAxis.Length)
+                        iterateAxis[0] = true;
+                    else
+                        iterateAxis[i + 1] = true;
 
-            //BWBWWWBWB
+                    break;
+                }
+                else if (iterateAxis[i] && !nextAxis)
+                {
+                    iterateAxis[i] = false;
+                    if (i == 0)
+                        iterateAxis[iterateAxis.Length - 1] = true;
+                    else
+                        iterateAxis[i - 1] = true;
 
+                    break;
+                }
+            }
         }
     }
 }
